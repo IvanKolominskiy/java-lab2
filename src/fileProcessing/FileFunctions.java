0c22a2782ec9cfe6c1382bb3b011470bb7240829
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2023-03-04 18:28:57 +0300

    calculateSymbols function has been added

--- src/fileProcessing/FileFunctions.java
+++ src/fileProcessing/FileFunctions.java
@@ -11,4 +11,25 @@ public class FileFunctions {
 
         return new Scanner(file);
     }
+
+    public int[] calculateSymbols(Scanner fileScanner) {
+        int largeSymbolsAmount = 0;
+        int smallSymbolsAmount = 0;
+
+        while (fileScanner.hasNextLine()) {
+            String buffer = fileScanner.nextLine();
+
+            for(int i = 0; i < buffer.length(); ++i) {
+                if ((int) buffer.charAt(i) >= (int) 'A' && (int) buffer.charAt(i) <= (int) 'Z') {
+                    largeSymbolsAmount += 1;
+                }
+
+                if ((int) buffer.charAt(i) >= (int) 'a' && (int) buffer.charAt(i) <= (int) 'z') {
+                    smallSymbolsAmount += 1;
+                }
+            }
+        }
+
+        return new int[]{smallSymbolsAmount, largeSymbolsAmount};
+    }
 }
