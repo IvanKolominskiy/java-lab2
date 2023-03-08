e953a95bfa86636e5059940916917e10f70d6231
Ivan Kolominskiy <ifkolominskiy@edu.hse.ru>
2023-03-08 22:06:24 +0300

    Comments have been added

--- src/fileProcessing/FileFunctions.java
+++ src/fileProcessing/FileFunctions.java
@@ -5,18 +5,42 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+/**
+ * Class for functions for working with files
+ */
 public class FileFunctions {
 
+    /**
+     * Opens the file for reading
+     *
+     * @param filePath the path to the file to read
+     * @return Scanner for further reading of data from the file
+     * @throws FileNotFoundException if no such file exists
+     */
     public static Scanner openToRead(String filePath) throws FileNotFoundException {
         File file = new File(filePath);
         return new Scanner(file);
     }
 
+    /**
+     * Opens the file for writing
+     *
+     * @param filePath the path to the file to write
+     * @return PrintWriter for further writing data to a file
+     * @throws FileNotFoundException If such a file does not exist and a new one cannot be created
+     */
     public static PrintWriter openToWrite(String filePath) throws FileNotFoundException {
         File file = new File(filePath);
         return new PrintWriter(file);
     }
 
+    /**
+     * Counts the number of large and small letters
+     *
+     * @param fileScanner Scanner for reading from a file
+     * @return an array in which [0] -> amount of small symbols
+     *                           [1] -> amount of large symbols
+     */
     public static int[] calculateSymbols(Scanner fileScanner) {
         int largeSymbolsAmount = 0;
         int smallSymbolsAmount = 0;
